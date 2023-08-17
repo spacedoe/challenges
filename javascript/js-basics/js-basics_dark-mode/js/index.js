8e2196e8d9abf2b92f337bbefe73da9f7a406c61
ANNA STEELE <anna.a.steele@outlook.com>
2023-08-17 21:35:00 +0200

    complete js-basics_dark-mode challenge

--- javascript/js-basics/js-basics_dark-mode/js/index.js
+++ javascript/js-basics/js-basics_dark-mode/js/index.js
@@ -1,3 +1,48 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+const darkButton = document.querySelector('[data-js="dark-mode-button"]');
+const lightButton = document.querySelector('[data-js="light-mode-button"]');
+const toggleButton = document.querySelector('[data-js="toggle-button"]');
+
+darkButton.addEventListener('click', function() {
+    bodyElement.classList.add('dark');
+    console.log('Dark button clicked!');
+})
+
+lightButton.addEventListener('click', function() {
+    bodyElement.classList.remove('dark');
+    console.log('Light button clicked!');
+})
+
+toggleButton.addEventListener('click', function(){
+    bodyElement.classList.toggle('dark');
+    console.log('Toggle button clicked!');
+})
+
+//alternative:
+
+
+// const darkMode = () => {
+//     bodyElement.classList.add('dark');
+//     console.log('Dark button clicked!')
+// }
+
+// darkButton.addEventListener('click', darkMode);
+
+
+// const lightMode = () => {
+//     bodyElement.classList.remove('dark');
+//     console.log('Light button clicked!');
+// }
+
+// lightButton.addEventListener('click', lightMode);
+
+
+// const ToggleMode = () => {
+//     bodyElement.classList.toggle('dark');
+//     console.log('Toggle button clicked!');
+// }
+
+// toggleButton.addEventListener('click', ToggleMode);
