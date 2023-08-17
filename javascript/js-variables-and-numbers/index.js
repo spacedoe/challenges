02688acd9ccd304a2962195e594bdb8947db4b89
ANNA STEELE <anna.a.steele@outlook.com>
2023-08-17 12:40:40 +0200

    complete js-tip-calculator challenge

---
+++ javascript/js-variables-and-numbers/index.js
@@ -0,0 +1,8 @@
+const mealCost = 50
+const tip = 0.05
+const tipAmount = mealCost * tip
+const totalCost = mealCost + tipAmount
+
+// console.log("the total cost of your bill is " + totalCost + " euros.")
+// or
+console.log(`the total cost of your bill is ${totalCost} euros`)
