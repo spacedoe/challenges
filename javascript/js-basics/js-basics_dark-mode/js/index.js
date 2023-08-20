6c721b1500abbfc749650ff5a9aea9686d80bfcc
ANNA STEELE <anna.a.steele@outlook.com>
2023-08-20 19:27:03 +0200

    syntax update accoding to the comments

--- javascript/js-basics/js-basics_dark-mode/js/index.js
+++ javascript/js-basics/js-basics_dark-mode/js/index.js
@@ -1,29 +1,26 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
-
 const darkButton = document.querySelector('[data-js="dark-mode-button"]');
 const lightButton = document.querySelector('[data-js="light-mode-button"]');
 const toggleButton = document.querySelector('[data-js="toggle-button"]');
 
-darkButton.addEventListener('click', function() {
+darkButton.addEventListener('click', () => {
     bodyElement.classList.add('dark');
     console.log('Dark button clicked!');
 })
 
-lightButton.addEventListener('click', function() {
+lightButton.addEventListener('click', () => {
     bodyElement.classList.remove('dark');
     console.log('Light button clicked!');
 })
 
-toggleButton.addEventListener('click', function(){
+toggleButton.addEventListener('click', () => {
     bodyElement.classList.toggle('dark');
     console.log('Toggle button clicked!');
 })
 
-//alternative:
-
-
+//alternative solution:
 // const darkMode = () => {
 //     bodyElement.classList.add('dark');
 //     console.log('Dark button clicked!')
