57ac4dee2d298b16df13f3d650b414fcebcb69e4
ANNA STEELE <anna.a.steele@outlook.com>
2023-08-21 12:33:14 +0200

    complete js-functions_functions-with-parameters-extra challenge

--- javascript/js-functions/js-functions_functions-with-parameters-extra/js/index.js
+++ javascript/js-functions/js-functions_functions-with-parameters-extra/js/index.js
@@ -6,7 +6,10 @@ console.clear();
    The formula is: square = number * number
 */
 
-function printSquare(number) {}
+function printSquare(number) {
+   const square = number * number
+   console.log (square)
+}
 
 printSquare(3);
 printSquare(5);
@@ -19,8 +22,13 @@ printSquare(5);
 
 // Uncomment the following function calls and implement the function printCircumference
 
-// printCircumference(4);
-// printCircumference(6);
+function printCircumference(radius) {
+   const circumference = 2 * Math.PI * radius
+   console.log(circumference)
+}
+
+printCircumference(4);
+printCircumference(6);
 
 /*
 3: We want to use a function which accepts the width and length of a rectangle
@@ -32,5 +40,10 @@ printSquare(5);
 
 // Uncomment the following function calls and implement the function printRectangleArea
 
-// printRectangleArea(5, 7);
-// printRectangleArea(3, 4));
+function printRectangleArea (width, length) {
+   const area = width * length;
+   console.log("The area of the rectangle is " + area)
+}
+
+printRectangleArea(5, 7);
+printRectangleArea(3, 4);
