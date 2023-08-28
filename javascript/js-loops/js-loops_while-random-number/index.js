7a9986bc08eb57490cb1bf4895aa25d5b829f48e
ANNA STEELE <anna.a.steele@outlook.com>
2023-08-28 17:06:49 +0200

    complete js-loops_while-random-number challenge

--- javascript/js-loops/js-loops_while-random-number/index.js
+++ javascript/js-loops/js-loops_while-random-number/index.js
@@ -2,13 +2,29 @@ console.clear();
 
 // `while` loop
 
-let number = 0;
-let count = 0;
+// let number = 0;
+// let count = 0;
 
-// --v-- write/change code here --v--
+// // --v-- write/change code here --v--
+// while (number <= 0.9) {
 
-// --^-- write/change code here --^--
+// count++;
+// number = Math.random();
 
-console.log(
-  `The number that ended the loop was ${number} and it took ${count} rounds to do this!`
-);
+// console.log(number);
+// }
+// // --^-- write/change code here --^--
+
+// console.log(
+//   `The number that ended the loop was ${number} and it took ${count} rounds to do this!`
+// );
+
+//Other example:
+let age = 10
+let count = 0
+
+while (age <= 34) {
+  console.log("age:" + age, "count:" + count)
+  age+=5
+  count++
+}
