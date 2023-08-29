6e690dabf1c94806775ecd1505a4538b505cd85b
ANNA STEELE <anna.a.steele@outlook.com>
2023-08-29 19:57:33 +0200

    complete js-array-methods_map challenge

--- javascript/js-array-methods/js-array-methods_map/index.js
+++ javascript/js-array-methods/js-array-methods_map/index.js
@@ -24,10 +24,47 @@ const cards = [
   },
 ];
 
-const lowerCaseAnswers = null; // ['as often as you like.', ...]
+// const lowerCaseAnswers = null; // ['as often as you like.', ...]
+const lowerCaseAnswers = cards.map((card) => {
+  return card.answer.toLowerCase();
+});
+console.log(lowerCaseAnswers);
 
-const questionsAndAnswersTogether = null; // ["How often can I use <header>? - As often as you like.", ...]
+// const questionsAndAnswersTogether = null; // ["How often can I use <header>? - As often as you like.", ...]
+const questionsAndAnswersTogether = cards.map((card) => {
+  return `${card.question} - ${card.answer}`;
+});
+console.log(questionsAndAnswersTogether);
 
-const questionAndAnswer = null; // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
+// const questionAndAnswer = null; // [{ question: 'How often can I use <header>?', answer: 'As often as you like.'}, {...}]
 
+const questionAndAnswer = cards.map((card) => {
+  const newObject = {
+    question: card.question,
+    answer: card.answer,
+  };
+  return newObject;
+
+  console.log(Object.keys(cards));
+});
+console.log(questionAndAnswer);
+
+
+//Additional:
+
+//to get all keys in an array of objects
+// cards.map(card => Object.keys(card)).forEach(keys => console.log(keys));
+
+// to get all the values in an array of objects
+// 
+
+
+// to get all keys and values in an array of objects
+// cards.map(card => Object.entries(card)).forEach(entry => console.log(entry));
+//  or:
+// const allKeysAndValues = cards.map((card) => {
+//   for (let key in cards) {
+//     console.log(cards[key]);
+//   }
+// });
 export { lowerCaseAnswers, questionsAndAnswersTogether, questionAndAnswer };
