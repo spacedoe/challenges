44129b3c39c0bf44d6f9cb199b915487875675c0
ANNA STEELE <anna.a.steele@outlook.com>
2023-09-01 09:30:44 +0200

    complete js-array-methods-2_fun-with-flags challenge

--- javascript/js-array-methods-2/js-array-methods-2_fun-with-flags/index.js
+++ javascript/js-array-methods-2/js-array-methods-2_fun-with-flags/index.js
@@ -8,11 +8,19 @@ queryInput.addEventListener("input", (event) => {
   container.innerHTML = "";
 
   const searchString = event.target.value;
+  // console.log(searchString);
 
-  const foundCountry = null;
+  const foundCountries = countries.filter((country) =>
+    country.name.toLowerCase().startsWith(searchString)
+  );
+  console.log("f", foundCountries);
 
-  if (foundCountry) {
-    const countryElement = Country(foundCountry);
-    container.append(countryElement);
+  if (foundCountries) {
+    foundCountries.forEach((foundCountry) => {
+      console.log(foundCountry);
+      const countryElement = Country(foundCountry);
+      container.append(countryElement);
+
+    })
   }
 });
