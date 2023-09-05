d3ea8b3797af67d613d921f373313e49ceed1fe0
ANNA STEELE <anna.a.steele@outlook.com>
2023-09-05 11:30:34 +0200

    complete js-unit-testing_get-animal challenge

--- javascript/js-unit-testing/js-unit-testing_get-animal/index.js
+++ javascript/js-unit-testing/js-unit-testing_get-animal/index.js
@@ -1,1 +1,15 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+  if (animal === "cats") {
+    return `I totally love ${animal}!`;
+  }
+
+  if (typeof animal === "string") {
+    return `I like ${animal}!`;
+  }
+
+  if (typeof animal === "undefined") {
+    return `I do not like animals at all!`;
+  }
+}
+
+console.log(getAnimal());
