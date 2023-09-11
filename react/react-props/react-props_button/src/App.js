9df79978aea7e3c1026b0255a36a5365d7357739
ANNA STEELE <anna.a.steele@outlook.com>
2023-09-11 20:39:56 +0200

    challenge in progress

--- react/react-props/react-props_button/src/App.js
+++ react/react-props/react-props_button/src/App.js
@@ -2,5 +2,23 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <>
+      <Button />
+      <Button />
+    </>
+  );
+}
+
+function Button({ color = "blue", disabled, text, onButtonClick }) {
+  return (
+    <button
+      onClick={onButtonClick}
+      disabled={disabled}
+      style={{ backgroundColor: color, color: "white" }}
+    >
+      {" "}
+      {text}
+    </button>
+  );
 }
