60857047eaa38855e2f7d109a2f75d5dd9e5c339
ANNA STEELE <anna.a.steele@outlook.com>
2023-09-11 16:50:30 +0200

    complete react-props_greeting challenge

--- react/react-props/react-props_greeting/src/App.js
+++ react/react-props/react-props_greeting/src/App.js
@@ -1,5 +1,19 @@
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return <Greeting name="Jan" />;
+}
+
+function Greeting({ name }) {
+
+  //Main task:
+  // return <h1>Hello, {name}!</h1>
+  
+
+  //Bonus task:
+  const coaches = ["Jan", "Marcell", "Klaus", "Yair", "Gemena"];
+  // return <h1>{coaches.includes(name) ? "Hello, Coach!" : `Hello, ${name}!`}</h1>
+
+  // Alternative solution:
+  return <h1>Hello, {coaches.includes(name) ? "Coach" : name}!</h1>
 }
