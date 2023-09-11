b88e255d7af2e46a9881cf53348d60120d1a58ee
ANNA STEELE <anna.a.steele@outlook.com>
2023-09-11 11:42:00 +0200

    complete react-basics_hello-world-article challenge

--- react/react-basics/react-basics_hello-world-article/src/App.js
+++ react/react-basics/react-basics_hello-world-article/src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return HelloWorldArticle();
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>My article</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.{" "}
+      </p>
+    </article>
+  );
 }
