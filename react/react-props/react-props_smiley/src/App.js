f4ce41597846d0407f929b4be998db780309890e
ANNA STEELE <anna.a.steele@outlook.com>
2023-09-11 17:25:45 +0200

    complete react-props_smiley challenge

--- react/react-props/react-props_smiley/src/App.js
+++ react/react-props/react-props_smiley/src/App.js
@@ -2,5 +2,14 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  
+  // returns Happy smiley:
+  return <Smiley isHappy />;
+
+  // returns Sad smiley:
+  // return <Smiley />;
+}
+
+function Smiley({ isHappy }) {
+  return <h1> {isHappy ? "😄" : "😔"}</h1>;
 }
