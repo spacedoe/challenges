b972aff3f65b8bc583314cee1463a1b59e902fa6
ANNA STEELE <anna.a.steele@outlook.com>
2023-09-11 23:04:48 +0200

    complete react-props_button challenge

--- react/react-props/react-props_button/src/App.js
+++ react/react-props/react-props_button/src/App.js
@@ -2,22 +2,35 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
+  function handleClick(message) {
+    console.log(message);
+  }
   return (
     <>
-      <Button />
-      <Button />
+      <Button
+        color="green"
+        disabled={false}
+        text="Safe Button"
+        // This way handleClick function is only executed once the button is clicked.
+        onButtonClick={() => handleClick("Safe button is clicked!")}
+      />
+      <Button
+        color="red"
+        disabled={false}
+        text="Danger Button"
+        onButtonClick={() => handleClick("Danger button is clicked!")}
+      />
     </>
   );
 }
 
-function Button({ color = "blue", disabled, text, onButtonClick }) {
+function Button({ color, disabled, text, onButtonClick }) {
   return (
     <button
       onClick={onButtonClick}
       disabled={disabled}
       style={{ backgroundColor: color, color: "white" }}
     >
-      {" "}
       {text}
     </button>
   );
