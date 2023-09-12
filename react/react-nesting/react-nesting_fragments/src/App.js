eb6ac57ec2a014776d46670fdd2452bea56950f2
ANNA STEELE <anna.a.steele@outlook.com>
2023-09-12 11:44:20 +0200

    complete react-nesting_fragments challenge

--- react/react-nesting/react-nesting_fragments/src/App.js
+++ react/react-nesting/react-nesting_fragments/src/App.js
@@ -3,11 +3,17 @@ import "./styles.css";
 import Box from "./components/Box";
 
 export default function App() {
+  return <main className="flex-container">
+    <Boxes/>
+  </main>;
+}
+
+function Boxes() {
   return (
-    <main className="flex-container">
+    <>
       <Box color="#007bff" />
       <Box color="#fc3" />
       <Box color="#ff3333" />
-    </main>
+    </>
   );
 }
