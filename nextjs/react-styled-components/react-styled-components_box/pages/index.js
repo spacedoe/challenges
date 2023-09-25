5dea5857159027544971b0b5731079c2aa1fdac6
ANNA STEELE <anna.a.steele@outlook.com>
2023-09-25 14:24:42 +0200

    complete react-styled-components_box challenge - not finished

--- nextjs/react-styled-components/react-styled-components_box/pages/index.js
+++ nextjs/react-styled-components/react-styled-components_box/pages/index.js
@@ -1,10 +1,18 @@
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import BoxWithStyledComponents from "../components/BoxWithStyledComponents.js";
+import styled from "styled-components";
+
+const Container = styled.div`
+display: flex;
+`
 
 export default function HomePage() {
   return (
-    <div>
+    <Container>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
-    </div>
+      <BoxWithStyledComponents/>
+      <BoxWithStyledComponents isBlack={true}/>
+    </Container>
   );
 }
