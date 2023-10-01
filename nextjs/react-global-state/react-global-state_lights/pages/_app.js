ce4b7fcecfb0a04a5c2ddfa01568ae82f535efc0
ANNA STEELE <anna.a.steele@outlook.com>
2023-10-01 21:43:18 +0200

    minor update of _app.js

--- nextjs/react-global-state/react-global-state_lights/pages/_app.js
+++ nextjs/react-global-state/react-global-state_lights/pages/_app.js
@@ -2,18 +2,18 @@ import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
 import { useState } from "react";
 
-const initialLights = [
-  { id: 1, room: "Living Room", isOn: false },
-  { id: 2, room: "Kitchen", isOn: false },
-  { id: 3, room: "Bedroom", isOn: false },
-  { id: 4, room: "Bathroom", isOn: false },
-  { id: 5, room: "Garage", isOn: false },
-  { id: 6, room: "Porch", isOn: false },
-  { id: 7, room: "Garden", isOn: false },
-  { id: 8, room: "Office", isOn: false },
-];
 
 export default function App({ Component, pageProps }) {
+  const initialLights = [
+    { id: 1, room: "Living Room", isOn: false },
+    { id: 2, room: "Kitchen", isOn: false },
+    { id: 3, room: "Bedroom", isOn: false },
+    { id: 4, room: "Bathroom", isOn: false },
+    { id: 5, room: "Garage", isOn: false },
+    { id: 6, room: "Porch", isOn: false },
+    { id: 7, room: "Garden", isOn: false },
+    { id: 8, room: "Office", isOn: false },
+  ];
   const [lights, setLights] = useState(initialLights);
   function handleToggle(lightId) {
     setLights(
