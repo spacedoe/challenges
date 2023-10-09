d52e96580a824895310dbeff5e1a028d5132a0c8
ANNA STEELE <anna.a.steele@outlook.com>
2023-10-09 17:26:32 +0200

    compelete backend-create_products challenge

--- Backend/backend-create/backend-create_products/pages/api/products/index.js
+++ Backend/backend-create/backend-create_products/pages/api/products/index.js
@@ -8,4 +8,18 @@ export default async function handler(request, response) {
     const products = await Product.find();
     return response.status(200).json(products);
   }
+
+  if (request.method === "POST") {
+    try{
+      const productData = request.body
+      await Product.create(productData)
+      response.status(201).json({status: "Product created"})
+
+    } catch (error){
+      console.log(error);
+      response.status(404).json({error: error.message})
+      
+
+    }
+  }
 }
