5a728dec38056fdc40dad334e2fcf0c0e6420ae1
ANNA STEELE <anna.a.steele@outlook.com>
2023-10-09 15:48:59 +0200

    initial commit

---
+++ Backend/backend-read/backend-read_products/pages/api/products/[id].js
@@ -0,0 +1,20 @@
+import dbConnect from "@/db/connect";
+import Product from "../../../db/models/Product"
+
+export default async function handler(request, response) {
+  await dbConnect()
+
+  if (request.method === "GET") {
+    const { id } = request.query;
+    const product = await Product.findById(id)
+
+    if (!product) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+  
+    response.status(200).json(product);
+  }
+
+ 
+
+}
